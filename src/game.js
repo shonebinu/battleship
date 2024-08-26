bbcac0b02d9df31be4300a60ac5b1663551617b5
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-26 15:27:04 +0530

    finish implementing the game

--- src/game.js
+++ src/game.js
@@ -7,52 +7,66 @@ class Game {
   constructor() {
     this.player1 = new Player("You", new Gameboard());
     this.player2 = new Player("Machine", new Gameboard());
-    this.currentPlayer = this.player2;
+    this.lastPlayer = this.player2;
 
     this.init();
   }
 
   init() {
-    this.setupDummyShips();
+    this.setupShips();
     this.renderBoards();
     this.addEventListeners();
   }
 
-  setupDummyShips() {
+  setupShips() {
     this.placeShips(this.player1);
     this.placeShips(this.player2);
   }
 
   placeShips(player) {
     const gameboard = player.getGameboard();
+    const usedCoords = new Set();
 
-    gameboard.placeShip(new Ship(2), [
-      [1, 1],
-      [1, 0],
-    ]);
-    gameboard.placeShip(new Ship(3), [
-      [2, 2],
-      [2, 3],
-      [2, 4],
-    ]);
-    gameboard.placeShip(new Ship(3), [
-      [2, 6],
-      [2, 7],
-      [2, 8],
-    ]);
-    gameboard.placeShip(new Ship(4), [
-      [4, 0],
-      [4, 1],
-      [4, 2],
-      [4, 3],
-    ]);
-    gameboard.placeShip(new Ship(5), [
-      [5, 7],
-      [6, 7],
-      [7, 7],
-      [8, 7],
-      [9, 7],
-    ]);
+    for (const len of [2, 3, 3, 4, 5]) {
+      let placed = false;
+
+      while (!placed) {
+        const x = getRandomInt(0, 10);
+        const y = getRandomInt(0, 10);
+        const direction = Math.random() < 0.5 ? "horizontal" : "vertical";
+
+        const coords = this.getCoordinates(x, y, len, direction);
+
+        if (this.canPlaceShip(coords, usedCoords)) {
+          gameboard.placeShip(new Ship(len), coords);
+          coords.forEach((coord) => usedCoords.add(coord.toString()));
+          placed = true;
+        }
+      }
+    }
+  }
+
+  getCoordinates(x, y, length, direction) {
+    const coords = [];
+
+    for (let i = 0; i < length; i++) {
+      if (direction === "horizontal") coords.push([x + i, y]);
+      else coords.push([x, y + i]);
+    }
+
+    return coords;
+  }
+
+  canPlaceShip(coords, usedCoords) {
+    return coords.every(([x, y]) => {
+      return (
+        x >= 0 &&
+        x < 10 &&
+        y >= 0 &&
+        y < 10 &&
+        !usedCoords.has([x, y].toString())
+      );
+    });
   }
 
   renderBoards() {
@@ -80,19 +94,18 @@ class Game {
 
     boxes.forEach((box) => {
       box.addEventListener("click", () => {
-        if (this.currentPlayer !== player) return;
+        if (this.lastPlayer !== player) return;
 
         const boxClass = box.getAttribute("class");
         if (boxClass && boxClass !== "ship") {
           window.alert("This block has been already targeted once.");
-          return;
         }
-        this.handleAttack(box, gameboard, playerHTML, player);
+        this.handleAttack(box, gameboard, player);
       });
     });
   }
 
-  handleAttack(box, gameboard, playerHTML, player) {
+  handleAttack(box, gameboard, player) {
     const x = Number(box.getAttribute("data-x"));
     const y = Number(box.getAttribute("data-y"));
 
@@ -102,7 +115,7 @@ class Game {
 
     setTimeout(() => {
       if (gameboard.isShipsSunk()) {
-        alert(`${player.name} won the game`);
+        alert(`${player.name === "You" ? "Machine" : "You"} won the game`);
         return;
       }
     }, 1);
@@ -116,9 +129,47 @@ class Game {
   }
 
   switchPlayer() {
-    this.currentPlayer =
-      this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    this.lastPlayer =
+      this.lastPlayer === this.player1 ? this.player2 : this.player1;
+
+    if (this.lastPlayer === this.player1) this.machineTurn();
   }
+
+  machineTurn() {
+    const player1Gameboard = this.player1.getGameboard();
+    const player1TakenShots = new Set(
+      player1Gameboard.getMissedShots().map((coord) => coord.toString()),
+    );
+
+    for (const coords of player1Gameboard.getScoredShots().values()) {
+      for (const co of coords) {
+        player1TakenShots.add(co.toString());
+      }
+    }
+
+    let placed = false;
+    while (!placed) {
+      const coord = [getRandomInt(0, 10), getRandomInt(0, 10)];
+
+      if (!player1TakenShots.has(coord.toString())) {
+        const [x, y] = coord;
+
+        this.handleAttack(
+          document.querySelector(`.player1 [data-x='${x}'][data-y='${y}']`),
+          player1Gameboard,
+          this.player1,
+        );
+
+        placed = true;
+      }
+    }
+  }
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
 export { Game };
